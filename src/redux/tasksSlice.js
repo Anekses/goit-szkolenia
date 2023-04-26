fddce49d53ff460daac0555ed8e3b4d9b005da7e
Anekses <adrian.rydzynski@gmail.com>
2023-04-26 20:42:00 +0200

    14 class part 2

--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -11,6 +11,14 @@ const handleRejected = (state, action) => {
     state.error = action.payload;
 }
 
+const isPendingAction = (action) => {
+    return action.type.endsWith("/pending");
+}
+
+const isRejectAction = (action) => {
+    return action.type.endsWith("/rejected")
+}
+
 const tasksSlice = createSlice({
     name: "tasks",
     initialState: {
@@ -18,33 +26,64 @@ const tasksSlice = createSlice({
         isLoading: false,
         error: null,
     },
-    extraReducers: {
-        [fetchTasks.pending]: handlePending,
-        [addTask.pending]: handlePending,
-        [toggleCompleted.pending]: handlePending,
-        [fetchTasks.rejected]: handleRejected,
-        [addTask.rejected]: handleRejected,
-        [toggleCompleted.rejected]: handleRejected,
-        [fetchTasks.fulfilled](state, action) {
-            state.isLoading = false;
-            state.error = null;
-            state.items = action.payload;
-        },
-        [addTask.fulfilled](state, action) {
-            state.isLoading = false;
-            state.error = null;
-            state.items.push(action.payload)
-        },
-        [toggleCompleted.fulfilled](state, action) {
-            state.isLoading = false;
-            state.error = null;
-            const index = state.items.findIndex(
-                task => task.id === action.payload.id
-            );
-            state.items[index].completed = !state.items[index].completed
-        },
-
+    extraReducers: (builder) => {
+        builder
+            // .addCase(fetchTasks.pending, handlePending)
+            // .addCase(addTask.pending, handlePending)
+            // .addCase(toggleCompleted.pending, handlePending)
+            // .addCase(fetchTasks.rejected, handleRejected)
+            // .addCase(addTask.rejected, handleRejected)
+            // .addCase(toggleCompleted.rejected, handleRejected)
+            .addCase(fetchTasks.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.error = null;
+                state.items = action.payload;
+            })
+            .addCase(addTask.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.error = null;
+                state.items.push(action.payload)
+            })
+            .addCase(toggleCompleted.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.error = null;
+                const index = state.items.findIndex(
+                    task => task.id === action.payload.id
+                );
+                state.items[index].completed = !state.items[index].completed
+            })
+            .addMatcher(isPendingAction, handlePending)
+            .addMatcher(isRejectAction, handleRejected)
+            .addDefaultCase((state, action) => {state.error = "someone use old function, fix it!"})
+        
     }
+    // extraReducers: {
+    //     [fetchTasks.pending]: handlePending,
+    //     [addTask.pending]: handlePending,
+    //     [toggleCompleted.pending]: handlePending,
+    //     [fetchTasks.rejected]: handleRejected,
+    //     [addTask.rejected]: handleRejected,
+    //     [toggleCompleted.rejected]: handleRejected,
+    //     [fetchTasks.fulfilled](state, action) {
+    //         state.isLoading = false;
+    //         state.error = null;
+    //         state.items = action.payload;
+    //     },
+    //     [addTask.fulfilled](state, action) {
+    //         state.isLoading = false;
+    //         state.error = null;
+    //         state.items.push(action.payload)
+    //     },
+    //     [toggleCompleted.fulfilled](state, action) {
+    //         state.isLoading = false;
+    //         state.error = null;
+    //         const index = state.items.findIndex(
+    //             task => task.id === action.payload.id
+    //         );
+    //         state.items[index].completed = !state.items[index].completed
+    //     },
+
+    // }
 });
 
 export const tasksReducer = tasksSlice.reducer
